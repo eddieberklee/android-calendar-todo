d580fc499928bd8837cd754db1a2f3d45d6c5d9a
Eddie Lee <eddieberklee@gmail.com>
2015-12-02 18:34:49 -0800

    updating style of summary text v1

--- app/src/main/java/com/compscieddy/calendar_todo/MainActivity.java
+++ app/src/main/java/com/compscieddy/calendar_todo/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -102,7 +103,8 @@ public class MainActivity extends AppCompatActivity implements AddDayItemInterfa
       }
 
       int itemsCount = listView.getCount();
-      summaryText.setText("count: " + String.valueOf(itemsCount));
+      // TODO: move out into formatted @string
+      summaryText.setText(Html.fromHtml("<u>" + String.valueOf(itemsCount) + " more..." + "</u>")); // todo: maybe even underline this here
 
       boolean isCollapsing = listView.getVisibility() == View.VISIBLE;
       listView.setVisibility(isCollapsing ? View.GONE : View.VISIBLE);
